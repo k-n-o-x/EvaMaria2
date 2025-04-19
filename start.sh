20a1075632add33234a1d43b7eb2c5d40c84a4d4
I <96487144+k-n-o-x@users.noreply.github.com>
2025-04-20 00:43:00 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,12 @@
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/k-n-o-x/EvaMaria2
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /EvaMaria2
+fi
+cd /EvaMaria2
+pip3 install -U -r requirements.txt
+echo "Starting Bot...."
+python3 bot.py
