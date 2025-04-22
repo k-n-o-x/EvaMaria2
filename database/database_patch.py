d18ae96ec9291ade62807f91751bced1aa1a7739
I <96487144+k-n-o-x@users.noreply.github.com>
2025-04-22 11:21:50 +0530

    Update database_patch.py

--- database/database_patch.py
+++ database/database_patch.py
@@ -1,7 +1,8 @@
 from motor.motor_asyncio import AsyncIOMotorClient
-from umongo import Instance
+from models import instance  # This is your MotorAsyncIOInstance
+
 from info import DATABASE_URI, DATABASE_NAME
 
 client = AsyncIOMotorClient(DATABASE_URI)
 db = client[DATABASE_NAME]
-instance = Instance(db)
+instance.init(db)  # <-- This is the correct way to bind the DB
